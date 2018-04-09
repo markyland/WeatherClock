85ab61759aaf1f6512cba73ebd2800508e0e7b2c
Mark Zeldis <mzeldis@walleyetrading.net>
2018-04-08 21:32:01 -0400

    initial checkin

--- src/WeatherClock.java
+++ src/WeatherClock.java
@@ -4,7 +4,10 @@ import com.phidget22.RCServo;
 import org.jdom.input.*;
 import org.jdom.*;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 /**
@@ -25,74 +28,68 @@ public class WeatherClock {
 
     int lastWeather=ERROR;
 
-    private static final int HOT=40;
-    private static final int STORM=55;
-    private static final int RAIN=70;
-    private static final int CLOUDY=87;
-    private static final int PCLOUDY=107;
-    private static final int SUNNY=120;
-    private static final int FOG=137;
-    private static final int WINDY=153;
-    private static final int RAIN_AND_SNOW=167;
-    private static final int SNOW=188;
-    private static final int COLD=200;
+    private static final int HOT=10;
+    private static final int STORM=28;
+    private static final int RAIN=43;
+    private static final int CLOUDY=59;
+    private static final int PCLOUDY=78;
+    private static final int SUNNY=93;
+    private static final int FOG=108;
+    private static final int WINDY=124;
+    private static final int RAIN_AND_SNOW=138;
+    private static final int SNOW=158;
+    private static final int COLD=174;
 
     public WeatherClock(){
-        codeMap.put(0, STORM);
-        codeMap.put(1, STORM);
-        codeMap.put(2, STORM);
-        codeMap.put(3, STORM);
-        codeMap.put(4, STORM);
-        codeMap.put(5, RAIN_AND_SNOW);
-        codeMap.put(6, RAIN_AND_SNOW);
-        codeMap.put(7, RAIN_AND_SNOW);
-        codeMap.put(8, RAIN);
-        codeMap.put(9, RAIN);
-        codeMap.put(10, RAIN);
-        codeMap.put(11, RAIN);
+        codeMap.put(1, SUNNY);
+        codeMap.put(2, SUNNY);
+        codeMap.put(3, PCLOUDY);
+        codeMap.put(4, PCLOUDY);
+        codeMap.put(5, FOG);
+        codeMap.put(6, CLOUDY);
+        codeMap.put(7, CLOUDY);
+        codeMap.put(8, CLOUDY);
+        codeMap.put(11, FOG);
         codeMap.put(12, RAIN);
-        codeMap.put(13, SNOW);
-        codeMap.put(14, SNOW);
-        codeMap.put(15, SNOW);
-        codeMap.put(16, SNOW);
-        codeMap.put(17, RAIN);
+        codeMap.put(13, RAIN);
+        codeMap.put(14, RAIN);
+        codeMap.put(15, STORM);
+        codeMap.put(16, STORM);
+        codeMap.put(17, STORM);
         codeMap.put(18, RAIN);
-        codeMap.put(19, FOG);
-        codeMap.put(20, FOG);
-        codeMap.put(21, FOG);
-        codeMap.put(22, FOG);
-        codeMap.put(23, WINDY);
-        codeMap.put(24, WINDY);
-        codeMap.put(25, COLD);
-        codeMap.put(26, CLOUDY);
-        codeMap.put(27, CLOUDY);
-        codeMap.put(28, CLOUDY);
-        codeMap.put(29, PCLOUDY);
-        codeMap.put(30, PCLOUDY);
-        codeMap.put(31, SUNNY);
-        codeMap.put(32, SUNNY);
+        codeMap.put(19, SNOW);
+        codeMap.put(20, SNOW);
+        codeMap.put(21, SNOW);
+        codeMap.put(22, SNOW);
+        codeMap.put(23, SNOW);
+        codeMap.put(24, RAIN_AND_SNOW);
+        codeMap.put(25, RAIN_AND_SNOW);
+        codeMap.put(26, RAIN_AND_SNOW);
+        codeMap.put(29, RAIN_AND_SNOW);
+        codeMap.put(30, HOT);
+        codeMap.put(31, COLD);
+        codeMap.put(32, WINDY);
         codeMap.put(33, SUNNY);
         codeMap.put(34, SUNNY);
-        codeMap.put(35, RAIN);
-        codeMap.put(36, HOT);
-        codeMap.put(37, STORM);
-        codeMap.put(38, STORM);
-        codeMap.put(39, STORM);
+        codeMap.put(35, PCLOUDY);
+        codeMap.put(36, PCLOUDY);
+        codeMap.put(37, FOG);
+        codeMap.put(38, CLOUDY);
+        codeMap.put(39, RAIN);
         codeMap.put(40, RAIN);
-        codeMap.put(41, SNOW);
-        codeMap.put(42, SNOW);
+        codeMap.put(41, STORM);
+        codeMap.put(42, STORM);
         codeMap.put(43, SNOW);
-        codeMap.put(44, PCLOUDY);
-        codeMap.put(45, STORM);
-        codeMap.put(46, SNOW);
-        codeMap.put(47, STORM);
+        codeMap.put(44, SNOW);
 
         try {
             servo = new RCServo();
 
-//            servo.openAny();
-//            servo.waitForAttachment();
-        } catch (PhidgetException e) {
+            servo.open(5000);
+
+            setTargetPosition(0);
+        }
+        catch (PhidgetException e) {
             e.printStackTrace();
 
             System.exit(0);
@@ -101,13 +98,25 @@ public class WeatherClock {
         Runtime.getRuntime().addShutdownHook(new Thread(){
             public void run() {
                 try {
-                    servo.setTargetPosition(ERROR);
+                    setTargetPosition(ERROR);
                     servo.close();
                 } catch (PhidgetException e) {
                     e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
                 }
             }
         });
+    }
+
+    public void setTargetPosition(double pos) {
+        try {
+            servo.setTargetPosition(pos);
+            servo.setEngaged(true);
+            Thread.sleep(4000);
+            servo.setEngaged(false);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void start(){
@@ -120,7 +129,7 @@ public class WeatherClock {
                         //AudioPlayer.player.start(new AudioStream(getClass().getResourceAsStream("/thunder.wav")));
                     }
 
-                    servo.setTargetPosition(weather);
+                    setTargetPosition(weather);
 
                     if (weather!=ERROR){
                         lastWeather=weather;
@@ -132,14 +141,18 @@ public class WeatherClock {
             }
         };
 
-        new Timer().schedule(t, 5000, 5*60*1000);
+        new Timer().schedule(t, 0, 1800000);
     }
 
     public int getWeather() {
         try {
-            Document doc = new SAXBuilder().build(new URL("http://weather.yahooapis.com/forecastrss?p=10025"));
+            String s = readStringFromURL("http://dataservice.accuweather.com/currentconditions/v1/2650_PC?apikey=wKhQuqGjs5mQatvpLwmR8UNvFXsLsknP");
 
-            String code=doc.getRootElement().getChild("channel").getChild("item").getChild("condition", Namespace.getNamespace("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0")).getAttributeValue("code");
+            String searchText="\"WeatherIcon\":";
+
+            int startIndex=s.indexOf(searchText)+searchText.length();
+
+            String code=s.substring(startIndex, s.indexOf(",", +startIndex));
 
             Integer ret=codeMap.get(Integer.parseInt(code));
 
@@ -154,6 +167,13 @@ public class WeatherClock {
         }
 
         return ERROR;
+    }
+
+    private static String readStringFromURL(String requestURL) throws IOException {
+        try (Scanner scanner = new Scanner(new URL(requestURL).openStream(), StandardCharsets.UTF_8.toString())){
+            scanner.useDelimiter("\\A");
+            return scanner.hasNext() ? scanner.next() : "";
+        }
     }
 
     public static void main(String args[]){
